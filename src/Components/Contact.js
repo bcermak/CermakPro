e85da58cd4522dc33407e60a8ab867acbf589927
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-07 15:11:09 -0600

    fixing buttons on landing/footer spacing

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,13 +5,12 @@ class Contact extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
+
     }
 
     return (
@@ -25,7 +24,7 @@ class Contact extends Component {
 						   {name}<br />
 						   {city}, {state} {zip}<br />
 						   <span>{phone}</span><br />
-                     <span>{email}</span>
+                    <a href="mailto:cermakprofessional@gmail.com"><span>{email}</span></a> 
 					   </p>
 				   </div>
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -40,8 +40,8 @@ class Header extends Component {
             <h3>{description}.</h3>
             <hr />
             <ul className="social">
-               <a href={linkedin} className="button btn project-btn"><i className="fa fa-linkedin"></i>LinkedIn</a>
-               <a href={github } target="_blank" className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+               <a href="https://www.linkedin.com/in/brandon-cermak-ab4219121/"target="_blank" className="button btn"><i className="fa fa-linkedin"></i>LinkedIn</a>
+               <a href={github} target="_blank" className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
             </ul>
          </div>
       </div>
