2b95fe5667e08e74e58c6188ff59cc12dec9395c
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-06 23:45:31 -0600

    working

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,7 +22,6 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; CermakPro</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
         </div>
