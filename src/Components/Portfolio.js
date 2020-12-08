be375dbfd474fac8842572e248cffede216512c3
Brandon Cermak <cermakprofessional@gmail.com>
2020-12-07 21:02:51 -0600

    projects section

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -12,45 +12,45 @@ class Portfolio extends Component {
   render() {
     return (
       <section id="portfolio">
-<div style={{display: 'block'}}className= "container">
-    <div className ="row projects">
-        <div className= "col 6">
-            <div style={{fontWeight: 'bold',fontSize: 'larger'}}>Progressive Budget Tracker</div>
-            <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/budgettracker"> GitHub </a> || <a style={{fontWeight: 'bold'}} href = "https://budgettrackerrrr.herokuapp.com"> Deployed </a></div>
-            <Image style ={{marginTop:'8px',border:'solid black'}} src={ProgressiveBudget} className ="primaryPic1"></Image>
-        </div>
-        <div className= "col 6">
-            <div style={{fontWeight: 'bold',fontSize: 'larger', marginBottom:'8px'}}> React Employee Directory </div>
-            <Image style ={{border:'solid black'}} src={ReactEmployeeDirectory} className ="primaryPic1"></Image>
-            <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/ReactEmployeeDirectory"> GitHub </a> || <a style={{fontWeight: 'bold'}} href = "https://user-directoryy.herokuapp.com/"> Deployed </a></div>
-        </div>    
-    </div>
-    <div className ="row projects">
-        <div className="col-md-6">
-            <div style={{fontWeight: 'bold',fontSize: 'larger', marginBottom:'8px'}}> Weather the Weather (WTW) </div>
-            <Image style ={{border:'solid black'}} src={WeatherApp} className ="primaryPic1"></Image>
-            <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/Weather-Dashboard"> GitHub </a> || <a style={{ fontWeight: 'bold'}} href = "https://bcermak.github.io/Weather-Dashboard/"> Weather Dashboard </a></div>
-        </div>
-        <div className="col-md-6">
-            <div style={{fontWeight: 'bold',fontSize: 'larger', marginBottom:'8px'}}> BrewsNDadJokes </div>
-            <Image style ={{border:'solid black'}} src={BrewsNDadJokes} className ="primaryPic1"></Image>
-            <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/BrewsNDadJokes"> GitHub </a> || <a style={{fontWeight: 'bold'}} href = "https://bcermak.github.io/BrewsNDadJokes/"> Deployed </a></div>
-        </div>
-    </div>
-    <div id = "projectsBottom" className ="row projects">
-        <div className= "col-md-6">
-            <div style={{fontWeight: 'bold',fontSize: 'larger', marginBottom:'8px'}}>Team Profiles Generator (CLI) </div>
-            <Image style ={{border:'solid black'}} src={TeamProfiles} className ="primaryPic1"></Image>
-            <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/Team-Profiles"> GitHub </a></div>
-        </div>  
-        <div className="col-md-6">
-            <div style={{fontWeight: 'bold',fontSize: 'larger', marginBottom:'8px'}}> Keep Austin Social </div>
-            <Image style ={{border:'solid black'}} src={KeepAustinSocial}></Image>
-            <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/KeepAustinSocial"> GitHub </a> || <a style={{fontWeight: 'bold'}} href = "https://keepaustinsocial.herokuapp.com/"> Deployed </a></div>
-        </div>
-    </div>
-    
-    </div>  
+        <div style={{display: 'block'}}className= "container">
+          <div className ="row projects">
+            <h2 style={{paddingBottom:'25px'}}>PROJECTS <i class="fas fa-toolbox"></i> <i class="fas fa-screwdriver"></i></h2>
+            <div className= "col 6">
+                <div style={{fontWeight: 'bold',fontSize: '25px'}}>Progressive Budget Tracker</div>
+                <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/budgettracker"> GitHub </a> || <a style={{fontWeight: 'bold'}} href = "https://budgettrackerrrr.herokuapp.com"> Deployed </a></div>
+                <Image style ={{marginTop:'8px', border:'solid black'}} src={ProgressiveBudget} className ="primaryPic1"></Image>
+            </div>
+            <div className= "col 6">
+                <div style={{fontWeight: 'bold', fontSize: '25px'}}> React Employee Directory </div>
+                <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/ReactEmployeeDirectory"> GitHub </a> || <a style={{fontWeight: 'bold'}} href = "https://user-directoryy.herokuapp.com/"> Deployed </a></div>
+                <Image style ={{marginTop:'8px', border:'solid black'}} src={ReactEmployeeDirectory} className ="primaryPic1"></Image>
+            </div>    
+          </div>
+            <div className ="row projects">
+                <div className="col 6">
+                    <div style={{fontWeight: 'bold',fontSize: '25px'}}> Weather the Weather (WTW) </div>
+                    <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/Weather-Dashboard"> GitHub </a> || <a style={{ fontWeight: 'bold'}} href = "https://bcermak.github.io/Weather-Dashboard/"> Weather Dashboard </a></div>
+                    <Image style ={{marginBottom:'8px', border:'solid black'}} src={WeatherApp} className ="primaryPic1"></Image>
+                </div>
+                <div className="col 6">
+                    <div style={{fontWeight: 'bold',fontSize: '25px'}}> BrewsNDadJokes </div>
+                    <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/BrewsNDadJokes"> GitHub </a> || <a style={{fontWeight: 'bold'}} href = "https://bcermak.github.io/BrewsNDadJokes/"> Deployed </a></div>
+                    <Image style ={{ marginBottom:'8px', border:'solid black'}} src={BrewsNDadJokes} className ="primaryPic1"></Image>
+                </div>
+            </div>
+            <div id = "projectsBottom" className ="row projects">
+                <div className= "col 6">
+                    <div style={{fontWeight: 'bold',fontSize: '25px'}}>Team Profiles Generator (CLI) </div>
+                    <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/Team-Profiles"> GitHub </a></div>
+                    <Image style ={{marginBottom:'8px', border:'solid black'}} src={TeamProfiles} className ="primaryPic1"></Image>
+                </div>  
+                <div className="col 6">
+                    <div style={{fontWeight: 'bold',fontSize: '25px'}}> Keep Austin Social </div>
+                    <div><a style={{fontWeight: 'bold'}} href = "https://github.com/bcermak/KeepAustinSocial"> GitHub </a> || <a style={{fontWeight: 'bold'}} href = "https://keepaustinsocial.herokuapp.com/"> Deployed </a></div>
+                    <Image style ={{marginBottom:'8px', border:'solid black'}} src={KeepAustinSocial}></Image>
+                </div>
+            </div>
+          </div>  
       </section>
     );
   }
